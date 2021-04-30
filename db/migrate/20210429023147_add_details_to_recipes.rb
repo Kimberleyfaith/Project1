5e3620b862c882bb8882511bc9391274a78cb7e7
Kimberley Osborn <kimberleyfaith@Kimberleys-MacBook-Pro.local>
2021-04-30 10:08:58 +1000

    Update db

--- db/migrate/20210429023147_add_details_to_recipes.rb
+++ db/migrate/20210429023147_add_details_to_recipes.rb
@@ -1,6 +1,6 @@
 class AddDetailsToRecipes < ActiveRecord::Migration[5.2]
   def change
-    add_column :recipes, :title, :text
+    change_column :recipes, :title, :text
     add_column :recipes, :image, :text
     add_column :recipes, :user_id, :integer
   end
